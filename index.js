f0a4dd2da50719a4c1592847777dc0c5c22fbd3b
Abhay999-web <singhabhaypratap609@gmail.com>
2026-06-09 12:10:04 +0530

    Add email sending functionality with nodemailer and integrate into chat agent

--- index.js
+++ index.js
@@ -2,6 +2,29 @@ import "dotenv/config"
 import readline from "readline/promises"
 import {ChatMistralAI} from '@langchain/mistralai'
 import { HumanMessage } from "@langchain/core/messages"
+import {tool} from "@langchain/core/tools"
+import { createAgent } from "langchain";
+import { sendEmail } from "./mail.service.js"
+import * as z from "zod";    //for define format
+
+
+
+const emailTool = tool(
+
+    sendEmail,               //return string
+    {
+        name: "emailTool",
+        description: "Use this tool to send an email",
+        schema: z.object({
+            to: z.string().describe("The recipient's email address"),
+            html: z.string().describe("The HTML content of the email"),
+            subject: z.string().describe("The subject of the email"),
+
+        })
+    }
+)
+
+
 
 
 const rl = readline.createInterface({ 
@@ -14,6 +37,11 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest",
 })
 
+const agent = createAgent({
+    model,
+    tools : [emailTool]
+})
+
 const messages = []  //for all chat history
 
 while(true){
@@ -21,12 +49,14 @@ while(true){
 
     messages.push(new HumanMessage(userInput))
 
-    const response = await model.invoke(messages)
+    const response = await agent.invoke({
+        messages
+    })
 
-    messages.push(response)
+    messages.push(response.messages[response.messages.length-1])
 
-
-    console.log(response.content)
+    console.log(response.messages[response.messages.length-1].content)
+   
 }
 
 
